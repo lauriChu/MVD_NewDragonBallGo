ef4c3d4d37c09afd799770ad90225447b9a08742
lauIboo <lgf@iboomobile.com>
2019-04-01 21:08:52 +0200

    - Init Scene completed

--- DragonBallGo/Assets/Scenes/IntroScene/InitController.cs
+++ DragonBallGo/Assets/Scenes/IntroScene/InitController.cs
@@ -6,32 +6,17 @@ using UnityEngine.Audio;
 
 public class InitController : MonoBehaviour {
 
-	// Use this for initialization
-	public AudioSource audio;
-	void Start () {
-		
-	}
-
-
- void Awake()
- {
-                     
-    audio.Play ();
-	DontDestroyOnLoad (audio);
-                                 
- }
 	// onClick Listener for Login button
 	public void onClickLogin() 
 	{
-		Debug.Log("login");
-		//SceneManager.LoadScene(mScene.LOGIN);
+		Debug.Log("Login");
 		LoadingManager.Shared.Show(SceneManager.LoadSceneAsync(mScene.LOGIN));
 	}
 
 	public void onClickRegister()
 	{
 		Debug.Log("Register");
-		SceneManager.LoadScene(mScene.REGISTER);
+		LoadingManager.Shared.Show(SceneManager.LoadSceneAsync(mScene.REGISTER));
 	}
 
 	public void onClickOptions()
